65ce1588fd1e52caa567c766fd856873b0537d9e
Laura Gomez diaz <lgomez-d@c1r7s4.42madrid.com>
2021-04-14 18:28:51 +0200

    funcionando --save

--- run_game.c
+++ run_game.c
@@ -6,7 +6,7 @@
 /*   By: lgomez-d <lgomez-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 17:38:40 by lgomez-d          #+#    #+#             */
-/*   Updated: 2021/04/13 19:16:04 by lgomez-d         ###   ########.fr       */
+/*   Updated: 2021/04/14 15:25:15 by lgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int load_image(t_game *data)
 
     if (data->window.win)
     {
-        //mlx_sync(MLX_SYNC_IMAGE_WRITABLE, data->window.img.img.img);
+        mlx_sync(MLX_SYNC_IMAGE_WRITABLE, data->window.img.img.img);
         move(data);
         img = &data->window.img.img; 
         if (firts_time || data->has_moved)
@@ -48,11 +48,10 @@ int load_image(t_game *data)
                 print_minimap(data);
             if (data->to_save)
                 save_bmp(data);
-            //mlx_do_sync(data->window.ptr);
+            mlx_do_sync(data->window.ptr);
         }
-        printf("No debería llegar aquí\n");
         mlx_put_image_to_window(data->window.ptr, data->window.win, img->img, 0, 0);
-        //mlx_sync(MLX_SYNC_WIN_FLUSH_CMD, data->window.win);
+        mlx_sync(MLX_SYNC_WIN_FLUSH_CMD, data->window.win);
     }
     return (0);
 }
@@ -67,7 +66,6 @@ int run_game(t_game *data)
     load_image(data);
     if (!data->to_save)
     {
-        printf("No debería llegar a los hooks\n");
         mlx_hook(data->window.win, 2, 1L << 0, press_key, data);
         mlx_hook(data->window.win, 3, 1L << 1, release_key, data);
         mlx_hook(data->window.win, EVENT_EXIT, 1L << 17, event_exit, data);
--- save_bmp.c
+++ save_bmp.c
@@ -1,80 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   save_bmp.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgomez-d <lgomez-d@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 17:47:22 by lgomez-d          #+#    #+#             */
+/*   Updated: 2021/04/14 17:53:28 by lgomez-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "includes/run_game.h"
 
-// 54 + 4 * width * height
-
-void save_as_char(unsigned char *str, int nbr)
+void	save_as_char(unsigned char *str, int nbr)
 {
-    str[0] = (unsigned char)(nbr);
+	str[0] = (unsigned char)(nbr);
 	str[1] = (unsigned char)(nbr >> 8);
 	str[2] = (unsigned char)(nbr >> 16);
 	str[3] = (unsigned char)(nbr >> 24);
 }
 
-void write_header(t_game *data, int fd, int file_size)
+void	write_header(t_game *data, int fd, int file_size)
 {
-    unsigned char *header;
-    //int file_size;
+	unsigned char	header[54];
 
-    //filesize = 54 + 4 * data->width * data->height;
-    header = ft_calloc(sizeof(char), 54);
-    has_been_created(header);
-    header[0] = (unsigned char)('B');
-    header[1] = (unsigned char)('M');
-    save_as_char(&header[2], file_size);
-    /*
-    save_as_char(&header[10], 54);
-    save_as_char(&header[14], 40);
-    */
-    header[10] = (unsigned char)54;
-    header[14] = (unsigned char)40;
-    save_as_char(&header[18], data->width);
-    save_as_char(&header[22], data->height);
-    header[27] = (unsigned char)1;
-    header[28] = (unsigned char)24;
-    if (write(fd, header, 54) < 0)
-        ft_errors("Error to write screenshop.bmp");
-    free(header);
-    header = 0;
+	ft_bzero(header, 54);
+	header[0] = (unsigned char)('B');
+	header[1] = (unsigned char)('M');
+	save_as_char(&header[2], file_size);
+	header[10] = (unsigned char)54;
+	header[14] = (unsigned char)40;
+	save_as_char(&header[18], data->width);
+	save_as_char(&header[22], data->height);
+	header[26] = (unsigned char)1;
+	header[28] = (unsigned char)24;
+	if (write(fd, &header, 54) < 0)
+		ft_errors("Error to write screenshop.bmp");
 }
 
-void write_image(t_game *data, int fd, int pad)
+void	write_image(t_game *data, int fd, int pad)
 {
-    int x;
-    int y;
-    int color;
+	int	x;
+	int	y;
+	int	color;
 
-    y = data->height;
-    while (y >= 0)
-    {
-        x = data->width - 1;
-        while (x >= 0)
-        {
-            color = my_mlx_pixel_get(&data->window.img, x, y);
-            if (write(fd, &color, 3) < 0)
-                ft_errors("Error to write screenshop.bmp");
-            if (write(fd, "\0\0\0", pad) < 0)
-                ft_errors("Error to write screenshop.bmp");
-            x--;
-        }
-        y--;
-    }
+	y = data->height;
+	while (y >= 0)
+	{
+		x = 0;
+		while (x < data->width)
+		{
+			color = my_mlx_pixel_get(&data->window.img, x, y);
+			if (write(fd, &color, 3) < 0)
+				ft_errors("Error to write screenshop.bmp");
+			x++;
+		}
+		if (write(fd, "\0\0\0", pad) < 0)
+			ft_errors("Error to write screenshop.bmp");
+		y--;
+	}
 }
 
-void save_bmp(t_game *data)
+void	save_bmp(t_game *data)
 {
-    int fd;
-    int pad;
-    int file_size;
+	int	fd;
+	int	pad;
+	int	file_size;
 
-    pad = (4 - (data->width * 3) % 4) % 4;
-    file_size = 54 + (3 * (data->width + pad) * data->height);
-    //fd = open("screenshop.bmp", O_WRONLY | O_CREAT | O_TRUNC | O_APPEND);
-    fd = open("screenshop.bmp", O_WRONLY | O_CREAT | O_TRUNC | O_APPEND,  0644);
-    if (fd < 0)
-        ft_errors("Can't open screenshop.bmp");
-    write_header(data, fd, file_size);
-    write_image(data, fd, pad);
-    close(fd);
-    printf("Se ha terminado de guardar la imagen\n");
-    event_exit(data);
+	pad = (4 - (data->width * 3) % 4) % 4;
+	file_size = 54 + ((3 * data->width + pad) * data->height);
+	fd = open("screenshot.bmp", O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0644);
+	if (fd < 0)
+		ft_errors("Can't open screenshop.bmp");
+	write_header(data, fd, file_size);
+	write_image(data, fd, pad);
+	close(fd);
+	printf("screamshot.bmp has been created in this directory\n");
+	event_exit(data);
 }
